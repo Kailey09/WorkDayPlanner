905a9b2bea0e14a970ab5c8885b4fbb6f0d91596
Kailey09 <kcoop1323@gmail.com>
2022-03-15 13:05:51 -0400

    added localstorage

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
+// localStorage.setItem("myKey", "myValue")
+
+// alert(localStorage.getItem("myKey"))
+
 var currentTime = document.getElementById("currentDay");
 var day = moment();
 
 
 currentTime.textContent = day.format('MMMM Do YYYY, h:mm:ss a');
+
+function clickHandler(event) {
+    // console.log(event.target.previousElementSibling.value);
+    console.log(event.target.previousElementSibling.previousElementSibling.textContent);
+
+    var key = event.target.previousElementSibling.previousElementSibling.textContent;
+    var value = event.target.previousElementSibling.value;
+
+    localStorage.setItem(key, value);
+}
+
+ var buttons = document.querySelectorAll("button");
+ console.log(buttons);
+ for(var i =0; i < buttons.length; i++) {
+     buttons[i].addEventListener("click", clickHandler );
+ }
+
+ var textAreas = document.querySelectorAll("textarea");
+ for(var i = 0; i < textAreas.length; i++) {
+     var savedKey = textAreas[i].previousElementSibling.textContent;
+     var savedText = localStorage.getItem(savedKey)
+     textAreas[i].value = savedText;
+
+
+     // load color
+     if(color) {
+        textAreas[i].classList.add("present")
+
+     } else if(color) {
+        textAreas[i].classList.add("future")
+
+     } else {
+        textAreas[i].classList.add("past")
+
+     }
+
+ }
 
  
